50e6973205ee15048df0dc65e4907a640578b620
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2022-05-07 21:02:13 +0545

    Enable Dark mode and Added categories

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,8 @@
 
 import React, { Component } from 'react'
+import {
+  Link
+} from "react-router-dom";
 
 export class NavBar extends Component {
   
@@ -7,8 +10,8 @@ export class NavBar extends Component {
 
     return (
         <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">NewsMonkey</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light">
+            <Link className="navbar-brand" to="/">NewsMonkey</Link>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span className="navbar-toggler-icon"></span>
               </button>
@@ -16,15 +19,15 @@ export class NavBar extends Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home</a>
+                        <Link className="nav-link" to="/">Home</Link>
                     </li>
-                    <li className="nav-item"><a className="nav-link" href="/business">Business</a> </li>
-                    <li className="nav-item"><a className="nav-link" href="/entertainment">Entertainment</a> </li>
-                    <li className="nav-item"><a className="nav-link" href="/general">General</a> </li>
-                    <li className="nav-item"><a className="nav-link" href="/health">Health</a> </li>
-                    <li className="nav-item"><a className="nav-link" href="/science">Science</a> </li>
-                    <li className="nav-item"><a className="nav-link" href="/sports">Sports</a> </li>
-                    <li className="nav-item"><a className="nav-link" href="/technology">Technology</a> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/business">Business</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/general">General</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/health">Health</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/science">Science</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link> </li>
                 </ul>
             </div>
         </nav>
